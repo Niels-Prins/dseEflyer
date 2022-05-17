3c59975d09e1fa6d73f6d39d2c4194418aeb3687
Max Snoodijk <max_snoodijk@live.nl>
2022-05-17 10:19:40 +0200

    MassMethod.

--- Mass/class_II.py
+++ Mass/class_II.py
@@ -1,10 +1,33 @@
 class MassMethods:
 
     def __init__(self):
-        self.aspect_ratio = 1
+        # Load factor attributes.
+        self.safety_margin = 1.5
+        self.load_factor = 1
+        self.load_factor_ultimate = self.load_factor * self.load_factor
+
+        # Class I attributes.
+        self.mass_empty = 1
+        self.mass_takeoff = 1
+
+        # Wing attributes.
+        self.wing_aspect_ratio = 1
+        self.wing_area = 1
+        self.wing_span = 1
+        self.wing_sweep_half = 1
+        self.wing_sweep_quarter = 1
+        self.wing_taper = 1
+        self.wing_t_to_c = 1
+        self.wing_t_max = 1
+
+        # Flight condition attributes.
+        self.velocity = 100
+        self.density = 200
+        self.pressure = (self.density / 2) * self.velocity ** 2
 
     def cessna(self):
         asdkandf = 5
+        sdkfnsdf = 5
 
     def raymer(self):
         mass = 122
