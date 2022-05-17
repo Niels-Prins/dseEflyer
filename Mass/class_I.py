00ce9bd09291fdadb0cb19f21c7d202329a05acf
cnieuwboer <cnieuwboer@student.tudelft.nl>
2022-05-17 09:59:23 +0200

    Class I values

---
+++ Mass/class_I.py
@@ -0,0 +1,12 @@
+# outputs from class I
+structure = 345  # [kg]
+Structure = 345  # [kg]
+Batteries = 308  # [kg]
+Motor = 60  # [kg]
+OEW_ref = 713  # [kg]
+margin = 143  # [kg]
+OEW = 855  # [kg]
+Pilot = 90  # [kg]
+MTOW_1 = 945  # [kg]
+Passenger = 90  # [kg]
+MTOW_2 = 1035  # [kg]
