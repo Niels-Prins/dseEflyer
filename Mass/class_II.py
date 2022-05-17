3856792efc33eaa56c4e219fe7e4446f3d93a7fa
Max Snoodijk <max_snoodijk@live.nl>
2022-05-17 10:00:00 +0200

    Literature added.

---
+++ Mass/class_II.py
@@ -0,0 +1,16 @@
+class MassMethods:
+
+    def __init__(self):
+        self.aspect_ratio = 1
+
+    def cessna(self):
+        asdkandf = 5
+
+    def raymer(self):
+        pass
+
+    def torenbeek(self):
+        pass
+
+    def usaf(self):
+        pass
