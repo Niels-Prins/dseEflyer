71f5d5efde56d60b93279ed73eafff59d9fe1035
Max Snoodijk <max_snoodijk@live.nl>
2022-05-20 16:48:00 +0200

    Corrections fixed.

--- Mass/class_I.py
+++ Mass/class_I.py
@@ -1,8 +1,20 @@
+# def class_I():
+#     structure = 360
+#     batteries = 210
+#     motor = 60
+#     margin = 0.2
+#     OEM = (1 + margin) * (structure + batteries + motor)
+#
+#     occupants = 180
+#     TOM = OEM + occupants
+#
+#     return OEM, TOM, batteries, motor, occupants
+
 def class_I():
-    structure = 360
-    batteries = 210
+    structure = 345
+    batteries = 365
     motor = 60
-    margin = 0.2
+    margin = 0.3
     OEM = (1 + margin) * (structure + batteries + motor)
 
     occupants = 180
