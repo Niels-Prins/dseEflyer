cc84fb5a8d89f0596e61ee4beb6cff6f6260af5d
Wessel Kruidenier <w.l.kruidenier@student.tudelft.nl>
2022-05-25 17:13:13 +0200

    final version for verification including power curve

--- Propulsion/Fan sizing.py
+++ Propulsion/Fan sizing.py
@@ -2,35 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 """"###############################################################################################################"""
-""""Constants"""
+""""Variables"""
 """"###############################################################################################################"""
-rho0 = 1.225  # [kg/m3]
+#constants
 kt = 1.032914
 kq = 0.1110977401
-Q = 113
+
+#velocities
 V_cl = 77.17  # [m/s]
 V_to = 28.29
 V_la = 33.438
-P_required = 323000  # [W]
 
-lst= []
+#Power values
+P_required = 322800  # [W]
+P_a = 364000
 
+#aircraft characteristics
+AR = 5.82
+S = 14.5
+e = 0.8
+g = 9.81
+MTOM = 1153 #kg
+MTOM_3g = 3* MTOM
+Cd0 = 0.025
+
+#altitudes
 alt_0 = 0
-alt_6000= 1828
+alt_6000 = 1828
 
-thr_thres_cl_0 = 2864
-thr_thres_cl_6000 = 2394
-thr_thres_to_0 = 1613
-thr_thres_to_6000 = 1348
-thr_thres_la_0 = 1812
-thr_thres_la_6000 = 1598
-
-rpmlower = 4000
-rpmupper = 8000
-rpmspacing = 100
-marginlower = 1
-marginupper = 1.1
-
+#desired D for calculations
+D = 0.63
 """"###############################################################################################################"""
 """"Definitions"""
 """"###############################################################################################################"""
@@ -45,8 +46,8 @@ def ISA(h):
     rho1 = (p / (R * T))
     return [T, p, rho1]
 
-def Massflow(alt,D,V):
-    return ISA(alt)[2] * np.pi * D ** 2 / 4 * V
+def Massflow(thr,V4,V):
+    return thr /(V4-V)
 
 def Thrust(kt,n,D,alt):
     return kt * n ** 2 * D ** 4 * ISA(alt)[2]
@@ -69,13 +70,35 @@ def V3(V,w):
 def V4(V, e_d):
     return V / e_d
 
+def Drag(V, MTOM, alt):
+    Cl = (MTOM * g) / (0.5 * ISA(alt)[2] * V ** 2 * S)
+    Cd = Cd0 + Cl ** 2 / (np.pi * AR * e)
+    return 0.5*ISA(alt)[2]*V**2*S*Cd
+
+def Preq_drag(V, MTOM, alt, D, e_d):
+    return 0.75 * Drag(V, MTOM, alt) * V + np.sqrt(((Drag(V, MTOM, alt)**2 * V**2)/16) + ((Drag(V, MTOM, alt)**3) / (ISA(alt)[2] *np.pi*e_d*D**2)))
+
 """"###############################################################################################################"""
-""""Loop for calculation"""
+""""Diameter and RPM calculation"""
 """"###############################################################################################################"""
-for D_cl in np.arange(0.4, 1, 0.01):
+# Constants for loop specific
+lst = []
+thr_thres_cl_0 = 2810
+thr_thres_cl_6000 = 2762
+thr_thres_to_0 = 1582
+thr_thres_to_6000 = 1555
+thr_thres_la_0 = 1876
+thr_thres_la_6000 = 1844
+rpmlower = 5000
+rpmupper = 8000
+rpmspacing = 100
+marginlower = 1
+marginupper = 1.08
+
+#for loop
+for D_cl in np.arange(0.55, 0.75, 0.01):
     for e_d in np.arange(0.8, 1.01, 0.01):
         for n_cl in np.arange(rpmlower/60, rpmupper/60, rpmspacing/60):
-            mflow_cl_0 = Massflow(alt_0, D_cl, V_cl)
             Thr_cl_0 = Thrust(kt,n_cl,D_cl,alt_0)
             Torque_cl_0 = Torque(kq,n_cl,D_cl,alt_0)
             Pr_cl_0 = Preq(alt_0, Thr_cl_0, D_cl, V_cl, e_d)
@@ -83,9 +106,9 @@ for D_cl in np.arange(0.4, 1, 0.01):
             Torque_power_cl_0 = P_torque(w_cl_0, Torque_cl_0)
             V_3_cl_0 = V3(V_cl,w_cl_0)
             V_4_cl_0 = V4(V_3_cl_0, e_d)
+            mflow_cl_0 = Massflow(Thr_cl_0, V_4_cl_0, V_cl)
             if marginlower * thr_thres_cl_0 <= Thr_cl_0 <= marginupper * thr_thres_cl_0 and  P_required <= Pr_cl_0 <= 1.002 * P_required:
                 for n_cl_2 in np.arange(rpmlower / 60, rpmupper / 60, rpmspacing / 60):
-                    mflow_cl_6000 = Massflow(alt_6000, D_cl, V_cl)
                     Thr_cl_6000 = Thrust(kt,n_cl_2,D_cl,alt_6000)
                     Torque_cl_6000 = Torque(kq,n_cl_2,D_cl,alt_6000)
                     Pr_cl_6000 = Preq(alt_6000, Thr_cl_6000, D_cl, V_cl, e_d)
@@ -93,9 +116,9 @@ for D_cl in np.arange(0.4, 1, 0.01):
                     Torque_power_cl_6000 = P_torque(w_cl_6000, Torque_cl_6000)
                     V_3_cl_6000 = V3(V_cl,w_cl_6000)
                     V_4_cl_6000 = V4(V_3_cl_6000, e_d)
+                    mflow_cl_6000 = Massflow(Thr_cl_6000, V_4_cl_6000, V_cl)
                     if marginlower *thr_thres_cl_6000 <= Thr_cl_6000 <= marginupper * thr_thres_cl_6000:
                         for n_to in np.arange(rpmlower / 60, rpmupper / 60, rpmspacing / 60):
-                            mflow_to_0 = Massflow(alt_0, D_cl, V_to)
                             Thr_to_0 = Thrust(kt,n_to,D_cl,alt_0)
                             Torque_to_0 = Torque(kq,n_to,D_cl,alt_0)
                             Pr_to_0 = Preq(alt_0,Thr_to_0, D_cl, V_to, e_d)
@@ -103,9 +126,9 @@ for D_cl in np.arange(0.4, 1, 0.01):
                             Torque_power_to_0 = P_torque(w_to_0, Torque_to_0)
                             V_3_to_0 = V3(V_to,w_to_0)
                             V_4_to_0 = V4(V_3_to_0, e_d)
+                            mflow_to_0 = Massflow(Thr_to_0, V_4_to_0, V_to)
                             if marginlower *thr_thres_to_0 <= Thr_to_0 <= marginupper * thr_thres_to_0:
                                 for n_to_2 in np.arange(rpmlower / 60, rpmupper / 60, rpmspacing / 60):
-                                    mflow_to_6000 = Massflow(alt_6000, D_cl, V_to)
                                     Thr_to_6000 = Thrust(kt,n_to_2,D_cl,alt_6000)
                                     Torque_to_6000 = Torque(kq,n_to_2,D_cl,alt_6000)
                                     Pr_to_6000 = Preq(alt_6000,Thr_to_6000, D_cl, V_cl, e_d)
@@ -113,9 +136,9 @@ for D_cl in np.arange(0.4, 1, 0.01):
                                     Torque_power_to_6000 = P_torque(w_to_6000, Torque_to_6000)
                                     V_3_to_6000 = V3(V_to,w_to_6000)
                                     V_4_to_6000 = V4(V_3_to_6000, e_d)
+                                    mflow_to_6000 = Massflow(Thr_to_6000, V_4_to_6000, V_to)
                                     if marginlower *thr_thres_to_6000 <= Thr_to_6000 <= marginupper * thr_thres_to_6000:
                                         for n_la in np.arange(rpmlower / 60, rpmupper / 60, rpmspacing / 60):
-                                            mflow_la_0 = Massflow(alt_0, D_cl, V_la)
                                             Thr_la_0 = Thrust(kt,n_la,D_cl,alt_0)
                                             Torque_la_0 = Torque(kq,n_la,D_cl,alt_0)
                                             Pr_la_0 = Preq(alt_0, Thr_la_0, D_cl, V_cl, e_d)
@@ -123,9 +146,9 @@ for D_cl in np.arange(0.4, 1, 0.01):
                                             Torque_power_la_0 = P_torque(w_la_0, Torque_la_0)
                                             V_3_la_0 = V3(V_la,w_la_0)
                                             V_4_la_0 = V4(V_3_la_0, e_d)
+                                            mflow_la_0 = Massflow(Thr_la_0, V_4_la_0, V_la)
                                             if marginlower *  thr_thres_la_0 <= Thr_la_0 <= marginupper *thr_thres_la_0:
                                                 for n_la_2 in np.arange(rpmlower / 60, rpmupper / 60, rpmspacing / 60):
-                                                    mflow_la_6000 = Massflow(alt_6000, D_cl, V_la)
                                                     Thr_la_6000 = Thrust(kt,n_la_2,D_cl,alt_6000)
                                                     Torque_la_6000 = Torque(kq,n_la_2,D_cl,alt_6000)
                                                     Pr_la_6000 = Preq(alt_6000, Thr_la_6000, D_cl, V_la, e_d)
@@ -133,6 +156,7 @@ for D_cl in np.arange(0.4, 1, 0.01):
                                                     Torque_power_la_6000 = P_torque(w_la_6000, Torque_la_6000)
                                                     V_3_la_6000 = V3(V_la,w_la_6000)
                                                     V_4_la_6000 = V4(V_3_la_6000, e_d)
+                                                    mflow_la_6000 = Massflow(Thr_la_6000, V_4_la_6000, V_la)
                                                     if  marginlower * thr_thres_la_6000 <= Thr_la_6000 <= marginupper * thr_thres_la_6000:
                                                         lst.append([D_cl,D_cl**2/4*np.pi, e_d, max(n_cl * 60, n_cl_2 * 60, n_to * 60,
                                                                  n_to_2 * 60, n_la * 60, n_la_2 * 60), min(n_cl * 60, n_cl_2 * 60, n_to * 60,
@@ -154,7 +178,7 @@ for D_cl in np.arange(0.4, 1, 0.01):
                                                                          Torque_power_to_6000, Torque_power_la_0,
                                                                          Torque_power_la_6000])
 
-df = pd.DataFrame(lst, columns=[ "Diameter", "Area","e_d", "Max RPM", 'Min RPM',
+lst_header = [ "Diameter", "Area","e_d", "Max RPM", 'Min RPM',
                                         "RPM_cl_0", "RPM_cl_6000","RPM_to_0", "RPM_to_6000","RPM_la_0", "RPM_la_6000",
                                         "P_cl_0", "P_cl_6000","P_to_0", "P_to_6000","P_la_0", "P_la_6000",
                                         "Thrust_cl_0", "Thrust_cl_6000","Thrust_to_0", "Thrust_to_6000",
@@ -169,8 +193,41 @@ df = pd.DataFrame(lst, columns=[ "Diameter", "Area","e_d", "Max RPM", 'Min RPM',
                                       "Exit_speed_V4_cl_0", "Exit_speed_V4_cl_6000", "Exit_speed_V4_to_0",
                                       "Exit_speed_V4_to_6000", "Exit_speed_V4_la_0", "Exit_speed_V4_la_6000",
                                       "Torque_power_cl_0", "Torque_power_cl_6000", "Torque_power_to_0", "Torque_power_to_6000",
-                                     "Torque_power_la_0","Torque_power_la_0"])
-
+                                     "Torque_power_la_0","Torque_power_la_0"]
+df = pd.DataFrame(lst, columns=lst_header)
 df.to_excel("Prop_sizing_final.xlsx", index=False)
-df_new = df.loc[326]
-print(df_new)
+
+# index = df.index
+# for id in range(0,len(df)):
+#     if 0.95 * D < df.iloc[id]["Diameter"] < 1.05 * D:
+#         break
+#         # number = index[df[id]["Diameter"]].to_list()
+# print(id)
+
+
+id = 50
+df_choice = pd.DataFrame(np.array([df.iloc[id]]), columns= lst_header)
+df_choice.to_excel("Propeller_choice.xlsx", index=False)
+
+# print(df.iloc[id])
+
+""""###############################################################################################################"""
+""""Power curves"""
+""""###############################################################################################################"""
+alt_power = 0
+lst_Preq = []
+for V in np.arange(20, 120, 0.1):
+    lst_Preq.append([Preq_drag(V, MTOM, alt_power, df.iloc[id]["Diameter"], df.iloc[id]["e_d"]), V])
+
+#plot the curve
+df_curve = pd.DataFrame(lst_Preq, columns = ["Power required", "Velocity"])
+plt.plot(df_curve["Velocity"], df_curve["Power required"], label = "Power required")
+plt.plot( df_curve["Velocity"],P_a*np.ones(len(lst_Preq)), label = "Power available")
+plt.xlabel("Velocity")
+plt.ylabel("Power required")
+plt.legend()
+plt.title("Power curve")
+plt.show()
+
+#write data in excel
+df_curve.to_excel("Power_curve.xlsx", index = False)
