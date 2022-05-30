cf2cb5bff55f75652082ea0698bee61d7fce1b4f
Wessel Kruidenier <w.l.kruidenier@student.tudelft.nl>
2022-05-30 14:26:01 +0200

    final version for verification including power curve

--- Propulsion/Driveshaft.py
+++ Propulsion/Driveshaft.py
@@ -4,14 +4,12 @@ import pandas as pd
 
 Tau_al = 206*10**6
 #Tau-steel =
-Tor = 800
-sf = 2
+Tor = 800 #nm
+sf = 2 #-
 shaft_length = 0.928 #m
-rho = 2710
-lst1 = []
-lst2 = []
+rho = 2710 #kg/m3
 lst = []
-lst3 = []
+
 
 def Polarmom(c1,c2):
     global J
@@ -28,28 +26,13 @@ for c1 in np.arange(0,0.03,0.0001):
         if c1 < c2:
             Polarmom(c1,c2)
             Torque(c2,J,Tau_al)
-            # print(T, "torque")
-            # print(J,"Polar")
-            # print(c1, "c1")
-            # print(c2, "c2")
             if Tor*0.99 * sf <= T <= Tor*1.01 * sf:
                 Area = np.pi*(c2**2-c1**2)
                 Volume = Area*shaft_length
                 mass = Volume*rho
-                lst1.append(c1)
-                lst2.append(c2)
-                lst3.append(Area)
-                lst.append(mass)
+                lst.append([c1,c2,Area, mass, Volume])
 
-
-
-
-
-df = pd.DataFrame(columns = ["c1", "c2", "Area","mass"])
-df["c1"] = lst1
-df["c2"] = lst2
-df["Area"] = lst3
-df["mass"] = lst
+df = pd.DataFrame(lst, columns = ["c1", "c2", "Area","mass", "Volume"])
 df.to_excel("Driveshaft.xlsx", index=False)
 print(df)
 
