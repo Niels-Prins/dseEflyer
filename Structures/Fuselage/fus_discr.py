97a512f9400e93385f9a13d4d7f12ecaa859df85
Niels Prins <nielsprins2@gmail.com>
2022-06-02 14:24:33 +0200

    finished idealization of constant cross-section part

--- Structures/Fuselage/fus_discr.py
+++ Structures/Fuselage/fus_discr.py
@@ -9,13 +9,12 @@ class Fuselage_structure:
     def __init__(
         self,
         point_spacing_cross: int = 5,
-        spacing_3d: int = 80,
+        spacing_3d: int = 100,
         stringer_spacing: float = 140,
-        stringer_thickness: float= 2.5, 
-        stringer_width: float=30,
-        stringer_height: float=30,
-        skin_thickness: float = 2
-        
+        stringer_thickness: float = 2.5,
+        stringer_width: float = 30,
+        stringer_height: float = 30,
+        skin_thickness: float = 2,
     ):
         self.spacingCross = point_spacing_cross
         self.spacing_3d = spacing_3d
@@ -24,89 +23,113 @@ class Fuselage_structure:
         self.stringerWidth = stringer_width
         self.stringerHeight = stringer_height
         self.skinThickness = skin_thickness
-        
+
+    def create_fuselage(self):  # Function to create the fuselage in one go
+        self.create_2d()
+        self.create_3d()
+        self.calc_b()
+        self.calc_inertiaZZ()
+
     @staticmethod
-    def calc_distance(x1, x2, y1, y2):
-        distance = np.sqrt(
-                    (x1 - x2) ** 2 + (y1 - y2) ** 2
-                )
+    def calc_distance(
+        x1, x2, y1, y2
+    ):  # Basic function to calculate distance between two points
+        distance = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
         return distance
 
-    def create_2d(self):
-        x_bot = [-400, 0, 400]
-        y_bot = [0, -620, 0]
-        bottomCurve = interpolate.interp1d(x_bot, y_bot, kind="quadratic")
+    def create_2d(self):  # Create 2d cross-section
+        # Data from Catia, interpolate curve through these points
+        y_bot = [-400, 0, 400]
+        z_bot = [0, -620, 0]
+        bottomCurve = interpolate.interp1d(y_bot, z_bot, kind="quadratic")
 
-        x_side = [400, 400 - 25.8141, 400 - 67.348, 400 - 109.619, 400 - 141.198, 250]
-        y_side = [0, 88.06, 188.067, 288.067, 402.401, 500]
-        side_curve = interpolate.interp1d(x_side, y_side, kind="cubic")
+        y_side = [400, 400 - 25.8141, 400 - 67.348, 400 - 109.619, 400 - 141.198, 250]
+        z_side = [0, 88.06, 188.067, 288.067, 402.401, 500]
+        side_curve = interpolate.interp1d(y_side, z_side, kind="cubic")
 
-        x_top = np.array([250, 0, -250])
-        y_top = [750, 1000, 750]
-        topCurve = interpolate.interp1d(x_top, y_top, kind="quadratic")
+        y_top = np.array([250, 0, -250])
+        z_top = [750, 1000, 750]
+        topCurve = interpolate.interp1d(y_top, z_top, kind="quadratic")
 
-        xCoorSide = []
-        yCoorSide = []
+        # Create empty coordinate lists for stringer/idealization
+        yCoorSide, zCoorSide = [], []
+        yCoorTop, zCoorTop = [], []
+        yCoorBot, zCoorBot = [], []
 
-        xCoorTop = []
-        yCoorTop = []
-
-        xCoorBot = []
-        yCoorBot = []
-        # Side curve spacing
-        oldySide = 500
-        oldyTop = 1000
-        oldyBot = -620
+        # Initilalize spacing
+        oldZSide = 500
+        oldZTop = 1000
+        oldZBot = -620
         distanceSide = 0
         distanceTop = 0
         distanceBot = 0
-        
-        xChecking = np.arange(0, 400, 0.01)
-        
-        for i in range(1, len(xChecking)):
-            if 0 <=xChecking[i] <= 250:
-                yTop = topCurve(xChecking[i])
-                distanceTop += Fuselage_structure.calc_distance(xChecking[i],xChecking[i - 1], yTop, oldyTop)
+
+        yChecking = np.arange(0, 400, 0.01)
+
+        # Function to place the stringers equally spaced around the  50% fuselage
+        for i in range(1, len(yChecking)):
+            if 0 <= yChecking[i] <= 250:
+                zTop = topCurve(yChecking[i])
+                distanceTop += Fuselage_structure.calc_distance(
+                    yChecking[i], yChecking[i - 1], zTop, oldZTop
+                )
                 if isclose(distanceTop, self.stringerSpacing, rel_tol=0.1):
-                    xCoorTop.append(xChecking[i])
-                    yCoorTop.append(yTop)
+                    yCoorTop.append(yChecking[i])
+                    zCoorTop.append(zTop)
                     distanceTop = 0
-                oldyTop = yTop
-                
-            elif 250 <= xChecking[i] <= 400:
-                ySide = side_curve(xChecking[i])
-                distanceSide += Fuselage_structure.calc_distance(xChecking[i],xChecking[i - 1], ySide, oldySide)
+                oldZTop = zTop
+
+            elif 250 <= yChecking[i] <= 400:
+                zSide = side_curve(yChecking[i])
+                distanceSide += Fuselage_structure.calc_distance(
+                    yChecking[i], yChecking[i - 1], zSide, oldZSide
+                )
                 if isclose(distanceSide, self.stringerSpacing, rel_tol=0.1):
-                    xCoorSide.append(xChecking[i])
-                    yCoorSide.append(ySide)
+                    yCoorSide.append(yChecking[i])
+                    zCoorSide.append(zSide)
                     distanceSide = 0
-                oldySide = ySide
-                
-            if 0 <= xChecking[i] <= 400:
-                yBot = bottomCurve(xChecking[i])
-                distanceBot += Fuselage_structure.calc_distance(xChecking[i],xChecking[i - 1], yBot, oldyBot)
-                
+                oldZSide = zSide
+
+            if 0 <= yChecking[i] <= 400:
+                zBot = bottomCurve(yChecking[i])
+                distanceBot += Fuselage_structure.calc_distance(
+                    yChecking[i], yChecking[i - 1], zBot, oldZBot
+                )
+
                 if isclose(distanceBot, self.stringerSpacing, rel_tol=0.1):
-                    xCoorBot.append(xChecking[i])
-                    yCoorBot.append(yBot)
+                    yCoorBot.append(yChecking[i])
+                    zCoorBot.append(zBot)
                     distanceBot = 0
-                oldyBot = yBot
-                
-        yCoorSide2 = np.arange(500, 750, 114)
-        xCoorSide2 = np.full(len(yCoorSide2), 250)
+                oldZBot = zBot
 
-        self.y = np.concatenate([xCoorBot, xCoorSide, xCoorSide2, xCoorTop])
-        self.z = np.concatenate([yCoorBot, yCoorSide, yCoorSide2, yCoorTop])
+        # Sort coordinates to loop counter clockwise around the fuselage
+        zCoorBot = [x for y, x in sorted(zip(yCoorBot, zCoorBot))]
+        zCoorSide = [x for y, x in sorted(zip(yCoorSide, zCoorSide))][::-1]
+        yCoorSide = yCoorSide[::-1]
 
-    def plot_cross_sec(self):
+        zCoorTop = [x for y, x in sorted(zip(yCoorTop, zCoorTop))][::-1]
+        yCoorTop = yCoorTop[::-1]
+
+        zCoorSide2 = np.arange(500, 750, 114)
+        yCoorSide2 = np.full(len(zCoorSide2), 250)
+
+        yHalf = np.concatenate([yCoorBot, yCoorSide, yCoorSide2, yCoorTop])
+        zHalf = np.concatenate([zCoorBot, zCoorSide, zCoorSide2, zCoorTop])
+
+        # Create full fuselage from 1 half
+        self.y = np.concatenate([yHalf, -yHalf])
+        self.z = np.concatenate([zHalf, zHalf])
+        self.stringer_num = len(self.z)
+
+    def plot_cross_sec(self):  # Plot 2d cross section
         plt.scatter(self.x, self.y, s=0.5)
         plt.axis("equal")
         plt.show()
 
-    def create_3d(self):
+    def create_3d(self):  # create 3d spacing of cross sections
         self.x = np.arange(0, 765 + self.spacing_3d, self.spacing_3d)
 
-    def plot_fuselage(self):
+    def plot_fuselage(self):  # Plot 3d fuselage
         self.create_3d()
         fig = plt.figure()
         ax = plt.axes(projection="3d")
@@ -116,27 +139,55 @@ class Fuselage_structure:
         ax.set_xlabel("x [mm]")
         ax.set_ylabel("y [mm]")
         ax.set_zlabel("z [mm]")
-        ax.set_ylim(0, 1620)
+        ax.set_ylim(-810, 810)
         ax.set_xlim(0, 1620)
         plt.show()
-        
-    def stringer_area(self):
-        self.stringerArea = self.stringerThickness *self.stringerWidth *2 + self.stringerThickness*(self.stringerHeight - 2*self.stringerThickness)
-        
-    def calc_b(self):
-        pass
-        
-        
+
+    def calc_stringer_area(self):  # Calculate area of a z-stringer
+        self.stringerArea = (
+            self.stringerThickness * self.stringerWidth * 2
+            + self.stringerThickness
+            * (self.stringerHeight - 2 * self.stringerThickness)
+        )
+
+    def calc_b(self):  # Calculate areas for idealization
+        self.calc_stringer_area()
+        areaslst = []
+        for idx in range(0, int(self.stringer_num / 2)):
+            b_further = Fuselage_structure.calc_distance(
+                self.y[idx], self.y[idx + 1], self.z[idx], self.z[idx + 1]
+            )
+            b_previous = Fuselage_structure.calc_distance(
+                self.y[idx], self.y[idx - 1], self.z[idx], self.z[idx - 1]
+            )
+            area = (
+                self.stringerArea
+                + (self.skinThickness * b_further / 2)
+                * (2 + self.y[idx + 1] / self.y[idx])
+                + (self.skinThickness * b_previous / 2)
+                * (2 + self.y[idx - 1] / self.y[idx])
+            )
+            areaslst.append(area)
+
+        reversed = areaslst[::-1]
+        self.areas = np.concatenate([areaslst, reversed])
+
+    def calc_neutralZ(
+        self,
+    ):  # Calculate the location of the neutral axis in z direction
+        self.neutralZ = sum(self.z * self.areas) / sum(self.areas)
+
+    def calc_inertiaZZ(
+        self,
+    ):  # Calculate the moment of inertia per boom and total of structure
+        self.calc_neutralZ()
+        self.inertias = self.areas * (self.neutralZ - self.z) ** 2
+        self.totalInertia = sum(self.inertias)
+
 
 #%%
-test = Fuselage_structure(10)
-test.create_2d()
-test.plot_fuselage()
 
-
-
-# %%
-
-# angle = atan(500/150)
-# x = cos(angle) * 114
-# %%
+if __name__ == "__main__":
+    test = Fuselage_structure(10)
+    test.create_fuselage()
+    test.plot_fuselage()
